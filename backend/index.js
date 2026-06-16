3404acff049e6215645d7eb55092d10a9ed6b49b
Subham Gayen <subhamgayen802@gmail.com>
2026-06-16 09:00:31 +0530

    eight commit

--- backend/index.js
+++ backend/index.js
@@ -33,7 +33,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin: process.env.FRONTEND_url || "http://localhost:5173",
 
     credentials: true
   },
